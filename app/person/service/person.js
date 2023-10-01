5257ec62a732c49245a1b219beb5e81ec77c6019
Dobberman <pangestubima89@gmail.com>
2023-10-01 09:56:48 +0700

    feature(app) : create person db repo and add queue

--- app/person/service/person.js
+++ app/person/service/person.js
@@ -5,17 +5,32 @@ class Person {
     #dbRepo;
     #emailProv;
     #jwt;
+    #queue;
 
-    constructor(dbRepo, emailProv, jwt) {
+    constructor(dbRepo, emailProv, jwt, queue) {
         this.#dbRepo = dbRepo;
         this.#emailProv = emailProv;
         this.#jwt = jwt;
+        this.#queue = queue;
     }
 
     async Register(name, email) {
         const person = await this.#dbRepo.Save(new PersonM({name, email, role: "client", state: "pending"}));
-        await this.#emailProv.Save(person);
+        await this.#queue.Push("person", "register", person);
         return person;
+    }
+
+    async Login(email) {
+        const persons = await this.#dbRepo.Load({email});
+        if (persons.IsEmpty()) {
+            throw new ErrNotFound("Person");
+        }
+        await this.#queue.Push("person", "register", persons.First());
+        return persons.First();
+    }
+
+    async SendMail(person){
+        return await this.#emailProv.Save(person);
     }
 
     async ConfirmPerson(token) {
@@ -27,13 +42,5 @@ class Person {
             person = (await this.#dbRepo.Load({ID})).First();
         }
         return this.#jwt.sign({ID: person.ID, command: "authToken"});
-    }
-
-    async Login(email) {
-        const persons = await this.#dbRepo.Load({email});
-        if (persons.IsEmpty()) {
-            throw new ErrNotFound("Person");
-        }
-        return this.#emailProv.Save(persons.First());
     }
 }
